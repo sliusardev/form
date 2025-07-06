3ffde36666e2f53a28c57a5dce50b3e4fb8ac2c8
sliusardev <sliusardev@gmail.com>
2025-07-06 13:21:47 +0300

    feat: refactor chart functionality and sidebar toggle into separate dashboard module

--- resources/views/dashboard/forms/index.blade.php
+++ resources/views/dashboard/forms/index.blade.php
@@ -2,19 +2,27 @@
 
 @section('content')
 
-    <div class="max-w-full mx-auto">
-        <div class="flex items-center justify-between mb-6">
+    <div class="max-w-full mx-auto bg-white p-4 rounded-lg shadow-md">
+        <div class="flex flex-wrap items-center justify-center lg:justify-between mb-6 gap-3">
             <h2 class="text-3xl font-semibold text-gray-800">Your Forms</h2>
-            <a href="{{ route('forms.create') }}" class="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 transition flex gap-2 items-center uppercase">
+            <a href="{{ route('forms.create') }}" class="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 transition flex gap-2 items-center uppercase text-center">
                 {{__('dashboard.create')}}
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-circle-plus"><circle cx="12" cy="12" r="10"/><path d="M8 12h8"/><path d="M12 8v8"/></svg>
             </a>
         </div>
 
-        <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+        <div class="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-6">
 
             @foreach($forms as $form)
-                <div class="bg-white rounded-lg shadow p-5">
+                <div class="bg-white rounded-lg shadow p-5 border border-gray-200 hover:shadow-lg transition-shadow duration-200">
+
+                    <div class="flex justify-between items-center mb-4">
+                        <h3 class="text-lg font-semibold text-gray-800">#{{ $form->id }}</h3>
+                        <span class="px-2 py-1 text-xs rounded-full {{ $form->is_enabled === true ? 'bg-green-100 text-green-800' : 'bg-yellow-100 text-yellow-800' }}">
+                            {{ $form->is_enabled ? ucfirst('Enabled') : ucfirst('Disabled') }}
+                        </span>
+                    </div>
+
                     <div class="border-b-gray-600">
                         <h3 class="text-xl font-semibold text-gray-800 mb-2">
                             <a href="" class="hover:underline">{{ $form->title }}</a>
@@ -68,21 +76,4 @@
 @endsection
 
 @push('scripts')
-{{--    <script>--}}
-{{--        function copyToClipboard(elementId) {--}}
-{{--            const text = document.getElementById(elementId).innerText;--}}
-{{--            navigator.clipboard.writeText(text)--}}
-{{--                .then(() => {--}}
-{{--                    const formId = elementId.split('-').pop();--}}
-{{--                    const tooltip = document.getElementById(`tooltip-${formId}`);--}}
-{{--                    tooltip.classList.add('opacity-100');--}}
-{{--                    setTimeout(() => {--}}
-{{--                        tooltip.classList.remove('opacity-100');--}}
-{{--                    }, 2000);--}}
-{{--                })--}}
-{{--                .catch(err => {--}}
-{{--                    console.error('Failed to copy: ', err);--}}
-{{--                });--}}
-{{--        }--}}
-{{--    </script>--}}
 @endpush
