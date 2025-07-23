df349e640b36f1e62bf1625bd01ae0d3d2ad132c
sliusardev <sliusardev@gmail.com>
2025-07-23 10:24:32 +0300

    fix: improve layout and overflow handling in formatted view for submissions

--- resources/views/dashboard/partials/submissions/formatted-view.blade.php
+++ resources/views/dashboard/partials/submissions/formatted-view.blade.php
@@ -1,19 +1,23 @@
 @foreach($submission->payload as $key => $value)
-
     @if(!is_array($value))
         <div class="grid grid-cols-1 md:grid-cols-3 gap-4 py-2 border-b border-gray-100">
             <div class="col-span-1 text-gray-700 font-medium md:border-r border-gray-100 bg-gray-50">
                 {{ ucwords(str_replace('_', ' ', $key)) }}
             </div>
-            <div class="col-span-2">{{ $value }}</div>
+            <div class="col-span-2 max-w-full overflow-x-auto break-words whitespace-normal">
+                {{ $value }}
+            </div>
         </div>
     @else
         <div class="grid grid-cols-1 md:grid-cols-3 gap-4 pb-2">
-            <div class="col-span-1 text-gray-600 font-medium">{{ ucwords(str_replace('_', ' ', $key)) }}</div>
+            <div class="col-span-1 text-gray-600 font-medium">
+                {{ ucwords(str_replace('_', ' ', $key)) }}
+            </div>
             <div class="col-span-2">
-                <pre class="text-sm bg-gray-50 p-2 rounded">{{ json_encode($value, JSON_PRETTY_PRINT) }}</pre>
+                <pre class="text-sm bg-gray-50 p-2 rounded overflow-x-auto">
+                    {{ json_encode($value, JSON_PRETTY_PRINT) }}
+                </pre>
             </div>
         </div>
     @endif
 @endforeach
-
