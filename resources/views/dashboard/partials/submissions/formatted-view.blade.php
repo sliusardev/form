b32f9e914781e3f05c5cd6c169cebeea8dc53f45
sliusardev <sliusardev@gmail.com>
2025-07-23 10:25:56 +0300

    fix: enhance overflow handling in formatted view for submissions

--- resources/views/dashboard/partials/submissions/formatted-view.blade.php
+++ resources/views/dashboard/partials/submissions/formatted-view.blade.php
@@ -1,7 +1,7 @@
 @foreach($submission->payload as $key => $value)
     @if(!is_array($value))
         <div class="grid grid-cols-1 md:grid-cols-3 gap-4 py-2 border-b border-gray-100">
-            <div class="col-span-1 text-gray-700 font-medium md:border-r border-gray-100 bg-gray-50">
+            <div class="col-span-1 text-gray-700 font-medium md:border-r border-gray-100 bg-gray-50 overflow-x-auto break-words whitespace-normal">
                 {{ ucwords(str_replace('_', ' ', $key)) }}
             </div>
             <div class="col-span-2 max-w-full overflow-x-auto break-words whitespace-normal">
