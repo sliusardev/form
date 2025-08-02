77fc22ba62245b74f64479ba6663647f93f05371
slusarvitaliy <slusarvitaliy@gmail.com>
2025-08-03 01:17:15 +0300

    fix: correct active class condition for payments link in sidebar

--- resources/views/dashboard/partials/sidebar.blade.php
+++ resources/views/dashboard/partials/sidebar.blade.php
@@ -64,7 +64,7 @@
                 {{__('dashboard.users')}}
             </a>
 
-            <a href="{{route('payments.index')}}" class="block px-4 py-2 hover:bg-gray-700 rounded flex items-center {{ request()->routeIs('users.index') ? 'bg-gray-700 font-bold' : '' }}">
+            <a href="{{route('payments.index')}}" class="block px-4 py-2 hover:bg-gray-700 rounded flex items-center {{ request()->routeIs('payments.index') ? 'bg-gray-700 font-bold' : '' }}">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="w-5 h-5 mr-3">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 10h18M7 15h1m4 0h1m-7 4h12a3 3 0 003-3V8a3 3 0 00-3-3H6a3 3 0 00-3 3v8a3 3 0 003 3z" />
                 </svg>
