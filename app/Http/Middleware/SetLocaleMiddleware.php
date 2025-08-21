9ee1a068b336ad709ae11d23decdddb809f3f95f
sliusardev <sliusardev@gmail.com>
2025-08-21 18:07:57 +0300

    feat: detect and set locale from browser languages if not already stored

--- app/Http/Middleware/SetLocaleMiddleware.php
+++ app/Http/Middleware/SetLocaleMiddleware.php
@@ -9,6 +9,21 @@ class SetLocaleMiddleware
 {
     public function handle($request, Closure $next)
     {
+        // If no locale stored yet, detect from browser
+        if (!Session::has('locale')) {
+            // Get all browser languages (e.g. ['uk-UA','uk','en-US','en'])
+            $browserLocales = array_map('strtolower', $request->getLanguages());
+
+            $locale = 'en'; // default
+            foreach ($browserLocales as $lang) {
+                if (str_starts_with($lang, 'uk')) {
+                    $locale = 'uk';
+                    break;
+                }
+            }
+            Session::put('locale', $locale);
+        }
+
         $locale = Session::get('locale', config('app.locale'));
         App::setLocale($locale);
         return $next($request);
